f156a799d822dfaf7b3a77c049bce8cfe1ea4a8d
Prashant Patil <prashant.patil@zoodpay.com>
2026-03-05 13:03:08 +0530

    Implement efficient O(n) anagram check using frequency map and count balancing

--- anagram.py
+++ anagram.py
@@ -1,33 +1,43 @@
-# First approach
-def is_anagram(str1, str2):
-    if len(str1) != len(str2):
+
+def is_anagram(a: str, b: str) -> bool:
+    """ Check if two strings are anagrams using frequency counting. """
+
+    if len(a) != len(b):
         return False
 
-    dict1 = {}
-    for char in str1:
-            dict1[char] = dict1.get(char, 0) + 1
+    count_chars = {}
+    for char in a:
+            count_chars[char] = count_chars.get(char, 0) + 1
 
-    for char in str2:
-        if dict1.get(char, 0) == 0:
+    for char in b:
+        if count_chars.get(char, 0) == 0:
             return False
-        dict1[char] -= 1
+        count_chars[char] -= 1
 
     return True
 
 
-print(is_anagram("evil", "lveli"))
+print(is_anagram("listen","silent"))
+print(is_anagram("triangle","integral"))
+print(is_anagram("evil","vill"))
+print(is_anagram("evil","lveli"))
 
-# Second approach
-def is_anagram_2(str1, str2):
-    if len(str1) != len(str2):
+
+def is_anagram_2(a: str, b: str) -> bool:
+    """ Check anagram using single pass balancing. """
+
+    if len(a) != len(b):
         return False
 
     count_chars = {}
-    for char in str1:
-        count_chars[str1[char]] = count_chars.get(char, 0) + 1
-        count_chars[str2[char]] = count_chars.get(char, 0) - 1
+    for a_char, b_char in zip(a, b):
+        count_chars[a_char] = count_chars.get(a_char, 0) + 1
+        count_chars[b_char] = count_chars.get(b_char, 0) - 1
 
     return all(value == 0 for value in count_chars.values())
 
 
-print(is_anagram_2("evil", "lveli"))
+print(is_anagram_2("listen","silent"))
+print(is_anagram_2("triangle","integral"))
+print(is_anagram_2("evil","vill"))
+print(is_anagram_2("evil","lveli"))
